eaaa1102215835fb106b32e73e8a2954499d71ba
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-15 15:21:09 +0530

    LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -6,7 +8,7 @@ export default function RootLayout({
     return (
         <div>
             <main>
-                <p className="text-white-1">LEFT SIDEBAR</p>
+                <LeftSidebar />
                 {children}
                 <p className="text-white-1">RIGHT SIDEBAR</p>
             </main>
