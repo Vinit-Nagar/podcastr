c9fdfa7c08bb6dac4d3bf28baa029337b8d53794
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-19 00:13:32 +0530

    clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://wondrous-coral-2.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+export default authConfig;
